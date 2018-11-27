be5eb8efb38cd775a3d5abb9714236a524c27eaf
zinleo <zin_leo@163.com>
2018-11-27 16:06:26 +0800

    document:drop console and comment

--- zTime/index.js
+++ zTime/index.js
@@ -2,7 +2,7 @@
  * @Author: Zin, LiaoZhiYong 
  * @Date: 2018-11-26 17:25:30 
  * @Last Modified by: Zin, LiaoZhiYong
- * @Last Modified time: 2018-11-26 21:43:54
+ * @Last Modified time: 2018-11-27 16:04:23
  */
 
 'use strict';
@@ -43,7 +43,6 @@ function fillIn(number) {
 exports.getMyTime = function (format = null, timestamp = null, area = 'EN') { 
   let now = timestamp ? new Date(timestamp - 0) : new Date();
   
-  console.log(format)
   if (!format) { return now; }
 
   let YY = now.getFullYear(),
@@ -67,83 +66,4 @@ exports.getMyTime = function (format = null, timestamp = null, area = 'EN') {
   REGEXP.YY.test(format) && (() => format = format.replace(REGEXP.YY, YY))();
 
   return format;
-
-  // switch (true) {
-  //   // year, format: YYYY | year | 年
-  //   case /^(YYYY|year|年)$/.test(format):
-  //     now = YY;
-  //     break;
-
-  //   // month, format: MM | month | 月
-  //   case /^(MM|month|月)$/.test(format):
-  //     now = MM;
-  //     break;
-    
-  //   // date, format: DD | date | 日
-  //   case /^(DD|date|日)$/.test(format):
-  //     now = DD;
-  //     break;
-
-  //   // date, format: YYYY-MM-DD | year-month-date | 年-月-日
-  //   case /^YYYY\-MM\-DD$/.test(format):
-  //   case /^year\-month\-date$/.test(format):
-  //   case /^年\-月\-日$/.test(format):
-  //     now = YY + '-' + MM + '-' + DD;
-  //     break;
-
-  //   // date, format: YYYYMMDD | yearMonthDate | 年月日
-  //   case /^YYYYMMDD$/.test(format):
-  //   case /^yearMonthDate$/.test(format):
-  //   case /^年月日$/.test(format):
-  //     now = YY + '' + MM + '' + DD;
-  //     break;
-
-  //   // hour, format: HH | hour | 时
-  //   case /^(HH|hour|时)$/.test(format):
-  //     now = HH;
-  //     break;
-
-  //   // minute, format: mm | minute | 分
-  //   case /^(mm|minute|分)$/.test(format):
-  //     now = mm;
-  //     break;
-
-  //   // second, format: ss | second | 秒
-  //   case /^(ss|second|秒)$/.test(format):
-  //     now = ss;
-  //     break;
-
-  //   // millisecond, format: ms | millisecond | 毫秒
-  //   case /^(ms|millisecond|毫秒)$/.test(format):
-  //     now = ms;
-  //     break;
-
-  //   // hour, minutes and second, format: HH:mm:ss | h:m:s | 时:分:秒
-  //   case /^(HH:mm:ss|h:m:s|时:分:秒)$/.test(format):
-  //     now = HH + ':' + mm + ':' + ss;
-  //     break;
-
-  //   // hour, minutes and second, format: HHmmss | hms | 时分秒
-  //   case /^(HHmmss|hms|时分秒)$/.test(format):
-  //     now = HH + '' + mm + '' + ss;
-  //     break;
-
-  //   // hour, minutes, second and millisecond, format: HH:mm:ss.ms | h:m:s.ms | 时:分:秒.毫秒
-  //   case /^(HH:mm:ss\.ms|h:m:s\.ms|时:分:秒\.毫秒)$/.test(format):
-  //     now = HH + ':' + mm + ':' + ss + '.' + ms;
-  //     break;
-
-  //   // year,month,date,hour,minute,second, format: (YYYY-MM-DD | year-month-date | 年-月-日) (HH:mm:ss | h:m:s | 时:分:秒)
-  //   case /^(YYYY\-MM\-DD|year\-month\-date|年\-月\-日) (HH:mm:ss|h:m:s|时:分:秒)$/.test(format):
-  //     now = YY + '-' + MM + '-' + DD + ' ' + HH + ':' + mm + ':' + ss;
-  //     break;
-
-  //   // year,month,date,hour,minute,second and millisecond, format: (YYYY-MM-DD | year-month-date | 年-月-日) (HH:mm:ss.ms | h:m:s.ms | 时:分:秒.毫秒)
-  //   case /^(YYYY\-MM\-DD|year\-month\-date|年\-月\-日) (HH:mm:ss\.ms|h:m:s\.ms|时:分:秒\.毫秒)$/.test(format):
-  //     now = YY + '-' + MM + '-' + DD + ' ' + HH + ':' + mm + ':' + ss + '.' + ms;
-  //     break;
-
-  //   default:
-  //     break;
-  // }
 }
